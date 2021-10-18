c85e54e2684a1092f2827d2c3db3956fff8db2ef
ingenius2021 <abhayaggrawal@pesu.pes.edu>
2021-10-18 12:20:31 +0530

    rectified

---
+++ js/countdown.js
@@ -0,0 +1,41 @@
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = 0;
+    hoursSpan.innerHTML = ('0' + 0).slice(-2);
+    minutesSpan.innerHTML = ('0' + 0).slice(-2);
+    secondsSpan.innerHTML = ('0' + 0).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
+var deadline = new Date("October 31, 2020 00:00:00");
+initializeClock('clockdiv', deadline);
